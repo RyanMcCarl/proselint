10ea631766fbd1b08e194ce0678999d53c3f48f5
Ryan McCarl <ryan.mccarl@gmail.com>
2018-07-05 00:43:18 -0700

    Changes

--- proselint/checks/passive_voice/passive_voice.py
+++ proselint/checks/passive_voice/passive_voice.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 """Check for constructions that may signal passive voice.
+# Resources:
+# https://writingcenter.unc.edu/tips-and-tools/passive-voice/
+# https://owl.english.purdue.edu/owl/owlprint/539/
 
 ---
 layout:     post
@@ -17,12 +20,11 @@ from proselint.tools import preferred_forms_check, existence_check, memoize
 
 
 @memoize
-def check_passive_voice(text):
+def check(text):
     """Check for constructions that may signal passive voice."""
     err = "passive_voice.passive_voice"
     msg = u"Check this sentence for passive voice; active voice is usually better."
-    regex = "(\b(?:be|am|is|are|was|were|have|has|had)\b[\w\s]{,15}?(?:d|(?<!whe)n|ne|left|being)\b(?: by\b)?)"
+    regex = r"(\b(?:be|am|is|are|was|were|have|has|had|get)\b[\w\s]{,15}?(?:d|(?<!whe)n|ne|left|being|made)\b(?: by\b)?)"
 
     return existence_check(text, [regex], err, msg,
-                    require_padding=False, offset=0)
-
+                           require_padding=False, offset=0)
---
+++ tests/test_passive_voice.py
@@ -0,0 +1,24 @@
+"""Tests for passive_voice.passive_voice check"""
+from __future__ import absolute_import
+
+from .check import Check
+
+from proselint.checks.passive_voice import passive_voice as chk
+
+
+
+class TestCheck(Check):
+    """The test class for passive_voice.passive_voice."""
+    __test__ = True
+
+    @property
+    def this_check(self):
+        """Boilerplate."""
+        return chk
+
+    @property
+    def test_smoke(self):
+        """Basic smoke test for passive_voice.passive_voice."""
+        assert self.passes("""This sentence is in the active voice.""")
+        assert not self.passes("""I was made to feel badly.""")
+        assert not self.passes("""My money was stolen by an ATM Machine.""")
