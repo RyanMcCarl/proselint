cbc8c0f4e89e0b0b9b9c4cb52ce739462c90c701
Ryan McCarl <ryan.mccarl@gmail.com>
2021-05-25 20:37:51 +0100

    Added simple passive voice checker.

---
+++ proselint/checks/passive_voice/misc.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Check for constructs that may signal passive voice.
+
+Resources:
+https://writingcenter.unc.edu/tips-and-tools/passive-voice/
+https://owl.english.purdue.edu/owl/owlprint/539/
+
+---
+layout:     post
+source:     Purdue OWL
+source_url: https://owl.english.purdue.edu/owl/owlprint/539/
+title:      Passive voice
+date:       2018-06-12 21:35:00
+categories: writing
+---
+
+"""
+
+from proselint.tools import existence_check, memoize
+
+
+@memoize
+def check(text):
+    """Check for constructs that may signal passive voice."""
+    err = "passive_voice.misc"
+    msg = u"This seems to be in passive voice. Consider using active voice."
+    regex = (r"(\b(?:be|am|is|are|was|were|have|has|had|get)"
+             r"\b[\w\s]{,15}?(?:d|(?<!whe)n|ne|left|being|made)"
+             r"\b(?: by\b)?)")
+
+    return existence_check(text, [regex], err, msg,
+                           require_padding=False, offset=0)
